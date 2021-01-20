48c8695f14ad793e042310c257549215ccb6c2b9
Cameron Croft <cameron.croft17032000@outlook.com>
2021-01-20 16:23:39 +0000

    reads tree

--- FamilyTreeTool/tree.cs
+++ FamilyTreeTool/tree.cs
@@ -27,11 +27,29 @@ namespace FamilyTreeTool
         public tree(string file)
         {
             InitializeComponent();
-
             ctree(file);
+
+            int GetHashCode()
+            {
+                int x =0;
+                if (tree1 != null)
+                {
+                    foreach (var p in tree1)
+                    {
+                        x = p.GetHashCode();
+                    }
+                }
+                return x;
+            }
+            
         }
 
         private void tree_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
